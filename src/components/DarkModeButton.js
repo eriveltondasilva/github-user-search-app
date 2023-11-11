7193e3dc6d7848cd016365a56b88dee63448b5b9
Erivelton Silva <eriveltondasilva13@gmail.com>
2023-11-11 03:21:36 -0300

    feat: cria botão modo dark

--- src/components/DarkModeButton.js
+++ src/components/DarkModeButton.js
@@ -1,7 +1,13 @@
-export default function DarkModeButton({ text, children }) {
+'use client';
+import { Moon, Sun } from 'lucide-react';
+import { useState } from 'react';
+
+export default function DarkModeButton() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <button>
-      {text} {children}
+    <button className='flex gap-3' onClick={() => setDarkMode(!darkMode)}>
+      <span>{darkMode ? 'DARK' : 'LIGHT'}</span>
+      {darkMode ? <Sun /> : <Moon />}
     </button>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import DarkModeButton from "@/components/DarkModeButton"
+
 export default function Header() {
   return (
     <header className='flex justify-between'>
       <div className='text-2xl font-bold'>devFinder</div>
-      <div>teste</div>
+      <div><DarkModeButton/></div>
     </header>
   );
 }
