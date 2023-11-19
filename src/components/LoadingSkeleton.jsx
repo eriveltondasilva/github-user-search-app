758085832c3bc2ec81d2d5c1b37717a4dc0b9416
Erivelton Silva <eriveltondasilva13@gmail.com>
2023-11-19 01:09:45 -0300

    feat: adiciona dark ao LoadingSkelet

--- src/components/LoadingSkeleton.jsx
+++ src/components/LoadingSkeleton.jsx
@@ -1,10 +1,10 @@
 export default function Loading() {
   return (
     <>
-      <div className='w-20 h-20 duration-100 rounded-full bg-slate-500 animate-pulse'></div>
+      <div className='w-20 h-20 duration-100 rounded-full bg-slate-600 dark:bg-slate-400 animate-pulse'></div>
       <div>
-        <div className='h-8 rounded-lg bg-slate-500 animate-pulse'></div>
-        <div className='my-6 rounded-lg sm:h-40 h-80 bg-slate-500 animate-pulse'></div>
+        <div className='h-8 rounded-lg bg-slate-600 dark:bg-slate-400 animate-pulse'></div>
+        <div className='my-6 rounded-lg sm:h-40 h-80 bg-slate-600 dark:bg-slate-400 animate-pulse'></div>
       </div>
     </>
   );
