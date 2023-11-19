3bc51446fe9af92320e1ec5adc4adae01d43e7a3
Erivelton Silva <eriveltondasilva13@gmail.com>
2023-11-19 19:05:46 -0300

    feat: adiciona autenticação

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Space_Mono } from 'next/font/google';
 import './globals.css';
 
+import ProviderContext from '@/contexts/ProviderContext';
 import UserProvider from '@/contexts/UserContext';
 
 const space_mono = Space_Mono({
@@ -19,13 +20,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en' className='dark'>
       <body className={space_mono.className}>
-        <UserProvider>
-          <div className='flex justify-center min-h-screen text-slate-950 dark:text-white bg-slate-100 dark:bg-slate-900'>
-            <div className='w-[24rem] sm:w-[36rem] mx-2 px-2 sm:px-4 my-8 sm:my-16'>
+        <ProviderContext>
+          <UserProvider>
+            <div className='flex justify-center min-h-screen text-slate-950 dark:text-white bg-slate-100 dark:bg-slate-900'>
               {children}
             </div>
-          </div>
-        </UserProvider>
+          </UserProvider>
+        </ProviderContext>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,9 +5,9 @@ export default function HomePage() {
   const userLogin = 'eriveltondasilva';
 
   return (
-    <>
+    <div className='w-[24rem] sm:w-[36rem] mx-2 px-2 sm:px-4 my-8 sm:my-16'>
       <Header userLogin={userLogin} />
       <MainCard />
-    </>
+    </div>
   );
 }
